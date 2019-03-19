609c91154b40d10d104edf2b48917ace13d49e92
ahauck74 <alec.hauck@valpo.edu>
2019-03-19 16:03:28 -0500

    Jackson changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json, request, redirect, url_for
+from flask import Flask, render_template, json, request, redirect, url_for, abort
 from forms import RerecForm
 import mysql.connector
 
@@ -11,15 +11,8 @@ mydb = mysql.connector.connect(
   database="senate"
 )
 
-clubs = {"101":"Alliance", 
-		"102": "Federation of Authors", 
-		"103": "The Insult Rosasco League",
-		"104": "The Grand Unified Hairline Project",
-		"0": "I have neither given or received, nor have I tolerated others' use of unauthorized aid."
-		}
-
 @app.route("/")
-def hello():
+def homepage():
 	return render_template("homepage.html")
 
 ############################
@@ -34,7 +27,7 @@ def new():
 ############################
 @app.route("/new-submission", methods=['POST', 'GET'])
 def new_submission():
-	#If the user tries to access this page by means other than the submit button, they are redirected to 		the form
+	#If the user tries to access this page by means other than the submit button, they are redirected to the form
 	if request.method == "GET":
 		return render_template("new_submission.html") 
 		
@@ -67,19 +60,65 @@ def new_submission():
 
 
 ############################
-# The Club Pages
+# The Org List
 ############################
-@app.route("/clubs")
-@app.route("/clubs/")
-@app.route("/clubs/<club>")
-def club_info(club=None):
-	#if no club is selected:
-	if(club == None):
-		return "<h1>Join a club!</h1>"
-		
+@app.route("/orgs")
+@app.route("/orgs/")
+def org_list():
+	cursor = mydb.cursor()
+	cursor.execute("SELECT ORG_NAME, ORG_ACR, ORG_EMAIL FROM organizations")
+	rows = cursor.fetchall()
 	
-	#Check if the club is in the list:
-	if club in clubs.keys():
-		return "<h2>This is where we'll have page info for " + clubs[club] + ".</h2>"
-	else:
-		return "<h2>Cannot find Club with ID: \"" + club + "\".</h2>"
+	names = []
+	ACRs = []
+	emails = []
+	
+	for row in rows:
+		name, ACR, email = row
+		names.append(name)
+		ACRs.append(ACR)
+		emails.append(email)
+	
+	#return str(names) + "<br>" + str(emails) + "<br>" + str(ACRs)
+
+	#Sawyer
+	try:
+		return render_template("club_short.html", NAMES=names, ACRS=ACRs, EMAILS=emails)
+	except:
+		abort(404)	
+		
+############################
+# Individual Org Page
+############################
+
+@app.route("/orgs/<ID>")
+def org_page(ID):
+	
+	"""
+	#Check the database for this org
+	cursor = mydb.cursor()
+	cursor.execute("SELECT ORG_NAME, ORG_ACR, ORG_EMAIL FROM organizations WHERE ID = " + str(ID))
+	rows = cursor.fetchall()
+	
+	#
+	#	Account for what happens if two ID's get returned <might not be necessary>
+	#
+	
+	#
+	#	Here is where I'll need to account for what will happen if the club ID isn't real.
+	#
+	
+	ID, name, acr, desc, email = row[0]
+	"""
+	
+	ID = 315
+	name = "Testy McTestClub"
+	ACR = "McTestClub"
+	desc = "I'm a testing platform for Sawyer's template. If you see me in production, something's gone horribly wrong."
+	email = "test@testymctestclub.valpo.edu"
+	
+	return render_template("club_full.html", ID=ID, NAME=name, ACR=ACR, DESC=desc, EMAIL=email)
+	
+	
+	
+
