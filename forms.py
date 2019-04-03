70f2dee2c8eb2337769fd962d296a1d38a5bbc44
ahauck74 <alec.hauck@valpo.edu>
2019-04-02 21:19:00 -0500

    Used js for dynamic officer/advisor fields

--- forms.py
+++ forms.py
@@ -5,21 +5,29 @@ class RecForm:
 		self.org_name = request.form['org_name'] 
 		self.org_email = request.form['org_email']
 		self.org_acronym = request.form['acronym']
+		self.tier = 'unfunded'
 		
-		#These fields will need to become arrays once we dress up the html to have a 
-		#variable number of officer fields
-		'''
-		self.name = request.form['name'] 
-		self.phone = request.form['phone'] 
-		self.email = request.form['email'] 
-		self.position = request.form['position']
-		'''
 		self.description = request.form['description']
 		self.events = request.form['events']
 		self.attendance = request.form['reg_attendance']
 		self.num_members = request.form['members_total']
 		
-		self.num_officers = request.form['num_officers']
+		self.num_officers = int(request.form['num_officers'])
+		self.off_names, self.off_phones, self.off_emails, self.off_pos = [], [], [], []
+		for i in range(self.num_officers):
+		
+			self.off_names.append(request.form['off_name' + str(i)])
+			self.off_phones.append(request.form['off_phone' + str(i)] )
+			self.off_emails.append(request.form['off_email' + str(i)] )
+			self.off_pos.append(request.form['off_pos' + str(i)] )
+		
+		self.num_advisors = int(request.form['num_advisors'])
+		self.adv_names, self.adv_phones, self.adv_emails = [], [], []
+		for i in range(self.num_officers):
+		
+			self.adv_names.append(request.form['adv_name' + str(i)])
+			self.adv_phones.append(request.form['adv_phone' + str(i)] )
+			self.adv_emails.append(request.form['adv_email' + str(i)] )
 		
 		
 		
@@ -37,8 +45,26 @@ class RecForm:
 			error_message += ("Number of officers needs to be an integer, instead got " + self.num_officers + ".\n")
 		if not isinstance(self.num_advisors, int):
 			error_message += ("Number of advisors needs to be an integer, instead got " + self.num_advisors + ".\n")'''
-		if (self.change_tier == 'y' and not self.tier_dest):
-			error_message += "Missing tier destination. Please select a tier or set change tier to no.\n"
+			
+		for i in range(self.num_officers):
+			if not (self.off_names[i]):
+				error_message += "Missing officer " + str(i+1) + " name.\n"
+			if not (self.off_phones[i]):
+				error_message += "Missing officer " + str(i+1) + " phone.\n"
+			if not (self.off_emails[i]):
+				error_message += "Missing officer " + str(i+1) + " email.\n"
+			if not (self.off_pos[i]):
+				error_message += "Missing officer " + str(i+1) + " position.\n"
+				
+		for i in range(self.num_advisors):
+			if not (self.adv_names[i]):
+				error_message += "Missing advisor " + str(i+1) + " name.\n"
+			if not (self.adv_phones[i]):
+				error_message += "Missing advisor " + str(i+1) + " phone.\n"
+			if not (self.adv_emails[i]):
+				error_message += "Missing advisor " + str(i+1) + " email.\n"
+				
+		
 		if len(error_message) == 0:
 			return None;
 		return error_message
@@ -56,29 +82,34 @@ class RerecForm:
 		self.org_email = request.form['org_email']
 		self.org_acronym = request.form['acronym']
 		
-		#These fields will need to become arrays once we dress up the html to have a 
-		#variable number of officer fields
-		'''
-		self.name = request.form['name'] 
-		self.phone = request.form['phone'] 
-		self.email = request.form['email'] 
-		self.position = request.form['position']
-		'''
-		self.is_recgonized = request.form['currently_recognized']#returns <y> or <n>
-		self.last_recognized = request.form['last_recognized']#This will likely be removed
-		self.cur_tier = request.form['tier']#<1>, <2>, <3>, or <null> for unfunded
 		self.change_tier = request.form['change_tier']#<y> or <n>
 		self.tier_dest = request.form['tier_destination']#<1>, <2>, <3>, or <null> for unfunded
 		self.description = request.form['description']
-		self.events = request.form['events']
 		self.attendance = request.form['reg_attendance']
 		self.num_members = request.form['members_total']
 		
 		self.num_officers = request.form['num_officers']
-
+		self.off_names, self.off_phones, self.off_emails, self.off_pos = [], [], [], []
+		for i in range(self.num_officers):
+		
+			self.off_names.append(request.form['off_name' + str(i)])
+			self.off_phones.append(request.form['off_phone' + str(i)] )
+			self.off_emails.append(request.form['off_email' + str(i)] )
+			self.off_pos.append(request.form['off_pos' + str(i)] )
+		
+		self.num_advisors = request.form['num_advisors']
+		self.adv_names, self.adv_phones, self.adv_emails = [], [], []
+		for i in range(self.num_officers):
+		
+			self.adv_names.append(request.form['adv_name' + str(i)])
+			self.adv_phones.append(request.form['adv_phone' + str(i)] )
+			self.adv_emails.append(request.form['adv_email' + str(i)] )
+		
+		
+		
 	def get_org_link(self):
 		return self.org_name.replace(" ","-")
-		
+
 	def validate(self):
 		error_message = ''
 		if not (self.org_name):
@@ -92,13 +123,29 @@ class RerecForm:
 			error_message += ("Number of advisors needs to be an integer, instead got " + self.num_advisors + ".\n")'''
 		if (self.change_tier == 'y' and not self.tier_dest):
 			error_message += "Missing tier destination. Please select a tier or set change tier to no.\n"
+		for i in range(self.num_officers):
+			if not (self.off_names[i]):
+				error_message += "Missing officer " + str(i+1) + " name.\n"
+			if not (self.off_phones[i]):
+				error_message += "Missing officer " + str(i+1) + " phone.\n"
+			if not (self.off_emails[i]):
+				error_message += "Missing officer " + str(i+1) + " email.\n"
+			if not (self.off_pos[i]):
+				error_message += "Missing officer " + str(i+1) + " position.\n"
+				
+		for i in range(self.num_advisors):
+			if not (self.adv_names[i]):
+				error_message += "Missing advisor " + str(i+1) + " name.\n"
+			if not (self.adv_phones[i]):
+				error_message += "Missing advisor " + str(i+1) + " phone.\n"
+			if not (self.adv_emails[i]):
+				error_message += "Missing advisor " + str(i+1) + " email.\n"
+				
+		
 		if len(error_message) == 0:
 			return None;
 		return error_message
 		
-	def commit_to_database(self):
-		#TODO: Write this method
-		print('hi')
 		
 class OfficerForm:
 	
@@ -152,13 +199,17 @@ class AdvisorForm:
 			if not (self.emails[i]):
 				error_message += "Missing advisor " + str(i+1) + " email.\n"
 		#TODO: Check if valid email using regex, and possibly add other validation requirements
-		'''if not isinstance(self.num_officers, int):
-			error_message += ("Number of officers needs to be an integer, instead got " + self.num_officers + ".\n")
-		if not isinstance(self.num_advisors, int):
-			error_message += ("Number of advisors needs to be an integer, instead got " + self.num_advisors + ".\n")'''
 
 		return error_message
 		
 		
-			
+class YeetSender:
+
+	def __init__(self):
+		print("Prepared to yeet!")
+	def deploy_yeet(self):
+		return "Yeet"
+	def deploy_n_yeets(self, n):
+		for i in range(n):
+			print(self.deploy_yeet())
 		
--- main.py
+++ main.py
@@ -26,8 +26,6 @@ def homepage():
 ##########################################
 @app.route("/new-rec", methods=['POST', 'GET'])
 def new_rec():
-	
-	
 	return render_template("recognition_form.html")
 	
 ##########################################
@@ -39,36 +37,39 @@ def new_rec_submission():
 	if request.method == "GET":
 		return render_template("recognition_form.html") 
 		
-	# read the posted values from the UI
+	
 	else:
+		# read the posted values from the form
 		form = RecForm(request)
 		errors = form.validate()
 		if (errors):
 			return(errors)
 		else:
 			
-			
 			mycursor = mydb.cursor()
-			sql = "INSERT INTO organizations (ORG_LINK_NAME, ORG_NAME, ORG_ACR, ORG_EMAIL, ORG_DESCRIPTION, CONSTITUTION, ORG_MEMBERS, ORG_ATTENDING_MEMBERS) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-			
-			
+			#Insert org info to the database
+			sql = """INSERT INTO organizations (ORG_LINK_NAME, ORG_NAME, ORG_ACR, ORG_EMAIL, ORG_DESCRIPTION, CONSTITUTION, ORG_MEMBERS, 			 
+					 ORG_ATTENDING_MEMBERS) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
 			val = (form.get_org_link(), form.org_name, form.org_acronym, form.org_email, form.description, None, form.num_members, form.attendance)
 			mycursor.execute(sql, val)
 			mydb.commit()
 			
-			#Get the ID of the newly created org
-			mycursor.execute("SELECT ORG_NAME, ORG_ID FROM organizations where ORG_NAME = " + form.org_name)
-			org_name_list = mycursor.fetchall()
+			ID = mycursor.lastrowid #The ID for the last insert done by this cursor object
 			
-			#TODO: The below code is a VERY roundabout way of formatting this list properly. The original fetchall  using just the org_name returns a list of tuples that looks like [('name1,'), ('name2,')... etc with the commas stuck there. This should be fixed.
-			names, IDs = [], []
-			for row in org_name_list:
-				name, ID = row
-				names.append(name)
-				IDs.append(ID)
+			sql_insert_query = """INSERT INTO officers (OFFICER_NAME, OFFICER_PHONE, OFFICER_EMAIL, TITLE, ORG_ID, YEAR) VALUES (%s, %s, %s, %s, %s, %s)"""
+			for i in range(form.num_officers):
+				val = (form.off_names[i], form.off_phones[i], form.off_emails[i], form.off_pos[i], ID, datetime.datetime.now().year)
+				mycursor.execute(sql_insert_query, val)
+			mydb.commit()
+			
+			sql_insert_query = """INSERT INTO advisors (ADVISOR_NAME, ADVISOR_PHONE, ADVISOR_EMAIL, ORG_ID, YEAR) VALUES (%s, %s, %s, %s, %s)"""
+			for i in range(form.num_advisors):
+				val = (form.adv_name[i], form.adv_phone[i], form.adv_email[i], ID, datetime.datetime.now().year)
+				mycursor.execute(sql_insert_query, val)
+			mydb.commit()
 			
 
-			return render_template('officer_form.html', ID=IDs[0], NUM_OFFICERS=form.num_officers)	
+			return redirect('orgs/'+ str(ID))
 
 ##########################################
 # The Rerec Form
@@ -81,21 +82,25 @@ def who_rerec():
 		mycursor.execute("SELECT ORG_NAME, ORG_ID FROM organizations")
 		org_name_list = mycursor.fetchall()
 		
-		#TODO: The below code is a VERY roundabout way of formatting this list properly. The original fetchall  using just the org_name returns a list of tuples that looks like [('name1,'), ('name2,')... etc with the commas stuck there. This should be fixed.
+		#TODO: The below code is a VERY roundabout way of formatting this list properly. The original fetchall  using just the org_name returns a list of 
+		#tuples that looks like [('name1,'), ('name2,')... etc with the commas stuck there. This should be fixed.
 		names, IDs = [], []
 		for row in org_name_list:
 			name, ID = row
 			names.append(name)
 			IDs.append(ID)
 		
-		return render_template("org_selection_form.html", ORGS = names, ) 
+		return render_template("org_selection_form.html", ORGS = names, IDS = IDs) 
 		
 	else:
 		mycursor = mydb.cursor()
-		mycursor.execute("SELECT ORG_NAME, ORG_ACR, ORG_EMAIL, ORG_ID FROM organizations WHERE ORG_NAME = '" + str(request.form['org']) + "'" )
+		sql = """SELECT ORG_NAME, ORG_ACR, ORG_EMAIL, ORG_ID, ORG_DESCRIPTION, CONSTITUTION, ORG_MEMBERS, ORG_ATTENDING_MEMBERS FROM organizations WHERE 
+		ORG_ID = '""" + str(request.form['org']) + "'" 
+		mycursor.execute(sql)
 		fetch = mycursor.fetchall()
-		#return(str(fetch[0]))
-		return render_template("rerecognition_form.html", NAME=fetch[0][0], ACR=fetch[0][1], EMAIL=fetch[0][2], ID=fetch[0][3])
+		
+		#TODO: Add officer/advisor info here, and make this cleaner.
+		return render_template("rerecognition_form.html", NAME=fetch[0][0], ACR=fetch[0][1], EMAIL=fetch[0][2], ID=fetch[0][3], DESC=fetch[0][4], CONSTITUTION=fetch[0][5], MEMBERS=fetch[0][6], ATTENDANCE=fetch[0][7])
 			
 
 ##########################################
@@ -107,8 +112,9 @@ def new_submission_rerec(ID):
 	if request.method == "GET":
 		return render_template("rerecognition_form.html", ID=ID) 
 		
-	# read the posted values from the UI
+	
 	else:
+		# read the user input into the RerecForm class
 		form = RerecForm(request)
 		errors = form.validate()
 		if (errors):
@@ -117,66 +123,24 @@ def new_submission_rerec(ID):
 			
 			mycursor = mydb.cursor()	
 			sql_update_query = """UPDATE organizations SET ORG_LINK_NAME = %s, ORG_NAME= %s, ORG_ACR= %s, ORG_EMAIL= %s, TIER_REQUEST= %s, ORG_DESCRIPTION= %s, CONSTITUTION= %s, ORG_MEMBERS= %s, ORG_ATTENDING_MEMBERS= %s WHERE ORG_ID = %s"""
-			
 			val = (form.get_org_link(), form.org_name, form.org_acronym, form.org_email, form.tier_dest, form.description, None, form.num_members, form.attendance, ID)
 			mycursor.execute(sql_update_query, val)
 			mydb.commit()
-
-			return render_template('officer_form.html', ID=ID, NUM_OFFICERS=int(form.num_officers))	
 			
-##########################################
-# The Submit Button For the Officer Form
-##########################################
-@app.route("/new-officers-submission/<ID>", methods=['POST', 'GET'])
-def new_officer_rerec(ID):
-	#If the user tries to access this page by means other than the submit button, they are redirected to the form
-	if request.method == "GET":
-		return render_template("officer_form.html", ID=ID) 
-		
-	# read the posted values from the UI
-	else:
-		form = OfficerForm(request)
-		errors = form.validate()
-		if (errors):
-			return(errors)
-		else:
-			
-			mycursor = mydb.cursor()
+			ID = mycursor.lastrowid #The ID for the last insert done by this cursor object
 			
 			sql_insert_query = """INSERT INTO officers (OFFICER_NAME, OFFICER_PHONE, OFFICER_EMAIL, TITLE, ORG_ID, YEAR) VALUES (%s, %s, %s, %s, %s, %s)"""
 			for i in range(form.num_officers):
-				val = (form.names[i], form.phones[i], form.emails[i], form.positions[i], ID, datetime.datetime.now().year)
+				val = (form.off_names[i], form.off_phones[i], form.off_emails[i], form.off_pos[i], ID, datetime.datetime.now().year)
 				mycursor.execute(sql_insert_query, val)
 			mydb.commit()
-
-			return render_template('advisor_form.html', ID=ID, NUM_ADVISORS=form.num_advisors)	
-			
-
-##########################################
-# The Submit Button For the Advisor Form
-##########################################
-@app.route("/new-advisors-submission/<ID>", methods=['POST', 'GET'])
-def new_advisor_rerec(ID):
-	#If the user tries to access this page by means other than the submit button, they are redirected to the form
-	if request.method == "GET":
-		return render_template("officer_form.html", ID=ID) 
-		
-	# read the posted values from the UI
-	else:
-		form = AdvisorForm(request)
-		errors = form.validate()
-		if (errors):
-			return(errors)
-		else:
-			
-			mycursor = mydb.cursor()
 			
 			sql_insert_query = """INSERT INTO advisors (ADVISOR_NAME, ADVISOR_PHONE, ADVISOR_EMAIL, ORG_ID, YEAR) VALUES (%s, %s, %s, %s, %s)"""
 			for i in range(form.num_advisors):
-				val = (form.name[i], form.phone[i], form.email[i], ID, datetime.datetime.now().year)
+				val = (form.adv_name[i], form.adv_phone[i], form.adv_email[i], ID, datetime.datetime.now().year)
 				mycursor.execute(sql_insert_query, val)
 			mydb.commit()
-
+			
 			return redirect('orgs/'+ str(ID))
 			
 
@@ -188,7 +152,6 @@ def new():
 	return render_template("advisor_officer_change_form.html") 
 
 
-		
 ##########################################
 # The Org List
 ##########################################
@@ -196,24 +159,25 @@ def new():
 @app.route("/orgs/")
 def org_list():
 	cursor = mydb.cursor()
-	cursor.execute("SELECT ORG_NAME, ORG_ACR, ORG_EMAIL FROM organizations")
+	cursor.execute("SELECT ORG_NAME, ORG_ACR, ORG_EMAIL, ORG_ID FROM organizations")
 	rows = cursor.fetchall()
-	#return str(rows)
 	names = []
 	ACRs = []
 	emails = []
+	IDS = []
 	
 	for row in rows:
-		name, ACR, email = row
+		name, ACR, email, ID = row
 		names.append(name)
 		ACRs.append(ACR)
 		emails.append(email)
+		IDS.append(ID)
 	
 	#return str(names) + "<br>" + str(emails) + "<br>" + str(ACRs)
 
 	#Sawyer
 
-	return render_template("org_list.html", NAMES=names, ACRS=ACRs, EMAILS=emails)
+	return render_template("org_list.html", NAMES=names, ACRS=ACRs, EMAILS=emails, IDS=IDS)
 
 		
 ##########################################
@@ -226,19 +190,16 @@ def org_page(ID):
 	
 	#Check the database for this org
 	cursor = mydb.cursor()
-	cursor.execute("SELECT ORG_NAME, ORG_ACR, ORG_EMAIL FROM organizations WHERE ORG_ID = " + str(ID))
+	cursor.execute("SELECT ORG_ID, ORG_NAME, ORG_ACR, ORG_DESCRIPTION, ORG_EMAIL FROM organizations WHERE ORG_ID = " + str(ID))
 	rows = cursor.fetchall()
 	
-	return str(rows)
-	#
-	#	Account for what happens if two ID's get returned <might not be necessary>
-	#
 	
-	#
-	#	Here is where I'll need to account for what will happen if the club ID isn't real.
-	#
 	
-	ID, name, acr, desc, email = row[0]
+	#Aborts (teapot) if someone tries to query an org that doesn't exist
+	if rows == []:
+		abort(418)
+	
+	ID, name, ACR, desc, email = rows[0]
 	
 	"""
 	ID = 315
