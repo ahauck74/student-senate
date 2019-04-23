8856b99c639a1230fbfe0c6e54c323c5d6b29135
ahauck74 <alec.hauck@valpo.edu>
2019-04-23 16:07:22 -0500

    Fixed filter archives by year

--- static/js/year_filter.js
+++ static/js/year_filter.js
@@ -1,14 +1,14 @@
-function yearFilter(year) {
+function yearFilter() {
 	var year = document.getElementById("year").value;
 	var table = document.getElementById("sort_table");
 	
-	rows = table.rows;
+	var rows = table.rows;
     /*Loop through all table rows (except the
     first, which contains table headers):*/
-    for (i = 1; i < (rows.length - 1); i++) {
-    	rows[i].type = 'hidden';
-    	if (rows[i].getElementsByTagName("TD")[4] == year) {
-    		rows[i].type = '';
+    for (i = 1; i < (rows.length); i++) {
+    	rows[i].style = 'display:none'; 
+    	if (rows[i].getElementsByTagName("TD")[4].innerText == year) {
+    		rows[i].style = '';
     	}
     }
 }
